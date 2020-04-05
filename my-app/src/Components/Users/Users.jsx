a9825de1199a0107d57b9fdb0cbb05a9217ec1ed
ivan-zhukouski <stigmatix.by@gmail.com>
2020-04-06 02:40:40 +0300

    fix Paginator

--- my-app/src/Components/Users/Users.jsx
+++ my-app/src/Components/Users/Users.jsx
@@ -3,37 +3,29 @@ import User from "./User";
 import Paginator from "../common/Paginator/Paginator";
 
 const Users = (props) => {
-    const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
-    const pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i)
-    }
-    const pageList = pages.map((p, index) => {
-        return (
-            <Paginator key={index}
-                       page={p}
-                       onPageChange={props.onPageChange}
-                       currentPage={props.currentPage} />
-        )
-    });
     const user = props.usersData.map(u => {
             return (
-               <User key={u.id} user={u}
-                     isFollowingProgress={props.isFollowingProgress}
-                     removeUser={props.removeUser}
-                     followUser={props.followUser} />
+                <User key={u.id} user={u}
+                      isFollowingProgress={props.isFollowingProgress}
+                      removeUser={props.removeUser}
+                      followUser={props.followUser}/>
             )
         }
     );
     return (
         <div>
-            <div className='d-flex m-2' style={{maxWidth: '500px'}}>
-                {pageList}
+            <Paginator onPageChange={props.onPageChange}
+                       currentPage={props.currentPage}
+                       totalUsersCount={props.totalUsersCount}
+                       pageSize={props.pageSize}/>
+            <div>
+                {user}
             </div>
-            {user}
-            <div className='d-flex m-2' style={{maxWidth: '500px'}}>
-                {pageList}
-            </div>
+
+            <Paginator onPageChange={props.onPageChange}
+                       currentPage={props.currentPage}
+                       totalUsersCount={props.totalUsersCount}
+                       pageSize={props.pageSize}/>
         </div>
     )
 };
--- my-app/src/Components/common/Paginator/Paginator.jsx
+++ my-app/src/Components/common/Paginator/Paginator.jsx
@@ -1,19 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import style from "../../Users/Users.module.css";
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import Button from '@material-ui/core/Button';
 
-const Paginator = ({page, onPageChange, currentPage}) => {
+const Paginator = ({onPageChange, currentPage, totalUsersCount, pageSize, portionSize = 10}) => {
+    const pagesCount = Math.ceil(totalUsersCount / pageSize);
+    const pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
+    }
+    const portionCount = Math.ceil(pagesCount / portionSize);
+    const [portionNumber, setPortionNumber] = useState(1);
+    const leftPortionNumber = (portionNumber - 1) * portionSize + 1;
+    const rightPortionNumber = portionNumber * portionSize;
+
     return (
-        <div
-             onClick={() => {
-                 onPageChange(page)
-             }}
-             className={`m-1 ${currentPage === page && style.currentPage}`}
-             style={{cursor: 'pointer'}}>
-            {
-                page <= 10 && page
+        <div className='d-flex m-2' style={{maxWidth: '500px'}}>
+            <div>
+                {portionNumber > 1 && <Button variant='contained' color="primary" onClick={() => {setPortionNumber(portionNumber - 1) }}>
+                    <NavigateBeforeIcon/>
+                </Button>}
+            </div>
+            {pages
+                .filter(p => p >= leftPortionNumber && p <= rightPortionNumber)
+                .map((p) => {
+                    return (
+                        <div
+                            key={p}
+                            onClick={() => {
+                                onPageChange(p)
+                            }}
+                            className={`m-1 ${currentPage === p && style.currentPage}`}
+                            style={{cursor: 'pointer'}}>
+                            {p}
+                        </div>
+                    )
+                })
             }
+            {portionCount > portionSize && <Button variant='contained' color="primary" onClick={()=> setPortionNumber(portionNumber + 1)}>
+                <NavigateNextIcon/>
+            </Button>}
         </div>
     )
 };
-
 export default Paginator
